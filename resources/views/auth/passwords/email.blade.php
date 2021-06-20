1f3140da06ea32406c72bfb4cba0b5819762354c
venixGit <69021103+venixGit@users.noreply.github.com>
2021-06-20 15:28:58 -0600

    feat: "vista olvidó contraseña agregado"

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -1,47 +1,28 @@
-@extends('layouts.app')
+@extends('layouts.template-auth')
 
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Reset Password') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    <form method="POST" action="{{ route('password.email') }}">
-                        @csrf
-
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Send Password Reset Link') }}
-                                </button>
-                            </div>
-                        </div>
-                    </form>
-                </div>
+@section('content-auth')
+        @if (session('status'))
+            <div class="alert alert-success" role="alert">
+            {{ session('status') }}
             </div>
-        </div>
-    </div>
-</div>
+        @endif
+
+    <form method="POST" action="{{ route('password.email') }}">
+        @csrf
+        <p class="mt-2 mb-0 pb-0 d-flex justify-content-between">¿Olvidó su contraseña? No hay problema, simplemente dejenos saber su dirección de correo electrónico y le enviaremos un enlace para restablecer una contraseña nueva.</p>
+        <!--INPUT DEL EMAIL-->
+        <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+        @error('email')
+            <span class="invalid-feedback" role="alert">
+                <strong>{{ $message }}</strong>
+            </span>
+        @enderror
+
+
+        <button type="submit" class="btn btn-info m-4 mx-auto">
+            {{ __('Send Password Reset Link') }}
+        </button>
+    </form> 
 @endsection
+
+
