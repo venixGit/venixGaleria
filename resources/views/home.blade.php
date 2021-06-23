fe890864385dddd0bfd47ee22a5839fbc8239136
venixGit <69021103+venixGit@users.noreply.github.com>
2021-06-22 19:18:43 -0600

    feat: "modal agregar articulo agregado"

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,11 +5,13 @@
         <div class="card shadow" onclick="showPhoto(this)">
             <img src="{{asset('img/fotos/img1.jpg')}}" class="card-img-top" alt="...">
             <div class="card-body">
-                <span class="badge badge-pill border border-info px-2 px-1 text-sans">#Decierto</span>
+                <span class="badge badge-pill border border-info px-2 px-1 text-sans">#Deciearto</span>
                 <span class="badge badge-pill border border-info px-2 px-1 text-sans">#Roca</span>
                 <span class="badge badge-pill border border-info px-2 px-1 text-sans">#Saára</span>
-
-                <p class="card-text mt-1 text-sans">Recuerdo de un gran dia.</p>
+                {{--   @foreach ($holas as $hola)
+                    <span class="badge badge-pill border border-info px-2 px-1 text-sans">{{$hola}}</span>
+                @endforeach --}}
+                <p class="card-text mt-1 text-sans">Recuerdo de un gran dia1.</p>
 
                 <p class="mt-2 mb-0 pb-0 d-flex justify-content-between">
                     <small class="text-muted">Hace 13 horas</small>
@@ -24,7 +26,7 @@
                 <span class="badge badge-pill border border-info px-2 px-1 text-sans">#Decierto</span>
                 <span class="badge badge-pill border border-info px-2 px-1 text-sans">#Roca</span>
                 <span class="badge badge-pill border border-info px-2 px-1 text-sans">#Saára</span>
-                <p class="card-text mt-1 text-sans">Recuerdo de un gran dia.</p>
+                <p class="card-text mt-1 text-sans">Recuerdo de un gran dia2.</p>
                 <p class="mt-2 mb-0 pb-0 d-flex justify-content-between">
                     <small class="text-muted">Hace 13 horas</small>
                 </p>
@@ -38,7 +40,7 @@
                 <span class="badge badge-pill border border-info px-2 px-1 text-sans">#Roca</span>
                 <span class="badge badge-pill border border-info px-2 px-1 text-sans">#Saára</span>
 
-                <p class="card-text mt-1 text-sans">Recuerdo de un gran dia.</p>
+                <p class="card-text mt-1 text-sans">Recuerdo de un gran dia3.</p>
 
                 <p class="mt-2 mb-0 pb-0 d-flex justify-content-between">
                     <small class="text-muted">Hace 13 horas</small>
@@ -65,112 +67,148 @@
 
 
     <!-- Modal new IMG-->
-    <div class="modal fade" id="newImg" tabindex="-1" role="dialog" aria-labelledby="newImgTitle" aria-hidden="true">
-      <div class="modal-dialog modal-dialog-centered modal-xl" role="document">
-        <div class="modal-content">
-          <div class="modal-header">
-            <h5 class="modal-title" id="newImgTitle">Nueva Foto</h5>
-            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-              <span aria-hidden="true">&times;</span>
-            </button>
-          </div>
-          <div class="modal-body">
-            <div class="row">
-                <div class="col-12 col-sm-12 col-md-12 col-lg-4 col-xl-4">
+    <form action="{{route('crearArticulos')}}" method="POST" enctype="multipart/form-data">
+        @csrf
+        <div class="modal fade" id="newImg" tabindex="-1" role="dialog" aria-labelledby="newImgTitle" aria-hidden="true">
+          <div class="modal-dialog modal-dialog-centered modal-xl" role="document">
+            <div class="modal-content">
+              <div class="modal-header">
+                <h5 class="modal-title" id="newImgTitle">Nueva Foto</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                  <span aria-hidden="true">&times;</span>
+                </button>
+              </div>
+              <div class="modal-body">
+                <div class="row">
+                    <div class="col-12 col-sm-12 col-md-12 col-lg-4 col-xl-4">
 
-                    <div class="input-group mb-3">
-                      <div class="custom-file">
-                        <input type="file" class="custom-file-input" id="imgNew" aria-describedby="inputGroupFileAddon04">
-                        <label class="custom-file-label" for="imgNew" data-browse="Buscar">Buscar Imagen</label>
-                      </div>
-                    </div>
-
-                    <img src="{{asset('img/app/blue_photo.svg')}}" class="img-fluid" alt="" style="max-height: 400px;">
-                    <!-- <img src="img/app/p1.jpg" alt="" style="height: 400px;"> -->
-
-                </div>
-                <div class="col-12 col-sm-12 col-md-12 col-lg-8 col-xl-8">
-                    <div class="row">
-                        <div class="col-12">
-                            <label for="txtTitulo" class="font-weight-normal">Titulo</label>
-                            <div class="input-group mb-3">
-                              <input type="text" class="form-control" id="txtTitulo" name="txtTitulo" aria-describedby="txtTitulo" placeholder="Escribe un titulo">
-                            </div>
-                        </div>  
-                        <div class="col-12">
-                            <label for="txtPlabrasClave" class="font-weight-normal">Palabras Clave</label>
-                            <div class="input-group mb-3">
-
-                              <input type="text" class="form-controls" id="txtPlabrasClave" name="txtPlabrasClave" value="" data-role="tagsinput" data-role="tagsinput" value="" placeholder="Palabra clave">
-                            </div>
+                        <div class="input-group mb-3">
+                          <div class="custom-file">
+                            <input  type="file" 
+                                    class="custom-file-input" 
+                                    id="imgNew"
+                                    name="imgNew"  
+                                    aria-describedby="inputGroupFileAddon04"
+                                    onchange="previewPhoto()">
+                            <label class="custom-file-label" for="imgNew" data-browse="Buscar">Buscar Imagen</label>
+                          </div>
                         </div>
-                        <div class="col-12">
-                            <label for="txtHistoria" class="font-weight-normal">Historia</label>
-                            <div class="input-group mb-3">
-                              <textarea name="txtHistoria" id="txtHistoria" name="txtHistoria" cols="30" rows="6" class="form-control" placeholder="Cuenta la historia de tu fotografia"></textarea>
+
+                        <img id="imgPhoto"src="{{asset('img/app/blue_photo.svg')}}" class="img-fluid" alt="" style="max-height: 400px;">
+                        <!-- <img src="img/app/p1.jpg" alt="" style="height: 400px;"> -->
+
+                    </div>
+                    <div class="col-12 col-sm-12 col-md-12 col-lg-8 col-xl-8">
+                        <div class="row">
+                            <div class="col-12">
+                                <label for="txtTitulo" class="font-weight-normal">Titulo</label>
+                                <div class="input-group mb-3">
+                                  <input    type="text" 
+                                            class="form-control" 
+                                            id="txtTitulo" 
+                                            name="txtTitulo" 
+                                            aria-describedby="txtTitulo" 
+                                            placeholder="Escribe un titulo"
+                                            value="{{old('titulo_articulo')}}"
+                                            required="">
+                                </div>
+                            </div>  
+                            <div class="col-12">
+                                <label for="txtPlabrasClave" class="font-weight-normal">Palabras Clave</label>
+                                <div class="input-group mb-3">
+
+                                  <input    type="text" 
+                                            class="form-controls" 
+                                            id="txtPalabrasClave" 
+                                            name="txtPalabrasClave" 
+                                            data-role="tagsinput" 
+                                            data-role="tagsinput" 
+                                            placeholder="Palabra clave"
+                                            required="" 
+                                           {{--  value="{{old('palabras_clave_articulo',$articulo->palabras_clave_articulo)}}" --}} >
+                                </div>
                             </div>
-                        </div>  
+                            <div class="col-12">
+                                <label for="txtHistoria" class="font-weight-normal">Historia</label>
+                                <div class="input-group mb-3">
+                                  <textarea     name="txtHistoria" 
+                                                id="txtHistoria" 
+                                                cols="30" 
+                                                rows="6" 
+                                                class="form-control" 
+                                                placeholder="Cuenta la historia de tu fotografia"
+                                                required="">
+                                               {{--  {{old('historia_articulo',$articulo->historia_articulo)} --}}  
+                                </textarea>
+                                </div>
+                            </div>  
+                        </div>
                     </div>
                 </div>
+              </div>
+              <div class="modal-footer">
+                <button type="button" class="btn btn-danger" data-dismiss="modal">Cerrar</button>
+                <button type="submit" class="btn btn-success" onclick="savePhoto()">Guardar</button>
+                @if(Session::has('message'))
+                    {!! Session::get('message') !!}
+                @endif
+              </div>
             </div>
           </div>
-          <div class="modal-footer">
-            <button type="button" class="btn btn-danger" data-dismiss="modal">Cerrar</button>
-            <button type="button" class="btn btn-success" onclick="savePhoto()">Guardar</button>
-          </div>
         </div>
-      </div>
-    </div>
+    </form>
 
     <!-- Modal show IMG-->
-    <div class="modal fade" id="showImg" tabindex="-1" role="dialog" aria-labelledby="newImgTitle" aria-hidden="true">
-      <div class="modal-dialog modal-dialog-centered modal-xl" role="document">
-        <div class="modal-content">
-          <div class="modal-header">
-            <h5 class="modal-title text-uppercase" id="showImgTitle">Detalle de Foto</h5>
-            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-              <span aria-hidden="true">&times;</span>
-            </button>
-          </div>
-          <div class="modal-body">
-            <div class="row">
-                <div class="col-12 col-sm-12 col-md-12 col-lg-4 col-xl-4">
-                    <img src="{{asset('img/fotos/img4.jpg')}}" class="img-fluid" alt="" style="height: 400px;">
-                </div>
-                <div class="col-12 col-sm-12 col-md-12 col-lg-8 col-xl-8">
-                    <div class="row">
-                        <div class="col-12">                 
-                             <h3 class="display-4" id="titleImg">Mi primer Viaje</h3>                       
-                        </div>
-                        <div class="col-12">
-                             <label id="fechaImg" class="text-muted"> <small>Hace 6 dias</small></label>
-                        </div>  
-                        <div class="col-12">
-                             <label  id="palabrasClave" >
-                                <span class="badge badge-pill border border-info px-2 py-1">Rio</span>
-                                <span class="badge badge-pill border border-info px-2 py-1">Lago</span>
-                                <span class="badge badge-pill border border-info px-2 py-1">Naturaleza</span>
-                              </label>
-                        </div>
-                        <div class="col-12">
-                        <hr class="m-0 p-0">
-                            <!-- <textarea class="text-sans mt-1" id="historyImg" ></textarea> -->
-                            <textarea name="pueb" id="" cols="30" rows="10" class="form-control text-sans">Un león que vagaba por el bosque se clavó una espina en la pata, y al encontrar un pastor, le pidió que se la extrajera. 
+    <form action="">
+        <div class="modal fade" id="showImg" tabindex="-1" role="dialog" aria-labelledby="newImgTitle" aria-hidden="true">
+          <div class="modal-dialog modal-dialog-centered modal-xl" role="document">
+            <div class="modal-content">
+              <div class="modal-header">
+                <h5 class="modal-title text-uppercase" id="showImgTitle">Detalle de Foto</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                  <span aria-hidden="true">&times;</span>
+                </button>
+              </div>
+              <div class="modal-body">
+                <div class="row">
+                    <div class="col-12 col-sm-12 col-md-12 col-lg-4 col-xl-4">
+                        <img src="{{asset('img/fotos/img4.jpg')}}" class="img-fluid" alt="" style="height: 400px;">
+                    </div>
+                    <div class="col-12 col-sm-12 col-md-12 col-lg-8 col-xl-8">
+                        <div class="row">
+                            <div class="col-12">                 
+                                 <h3 class="display-4" id="titleImg">Mi primer Viaje1</h3>                       
+                            </div>
+                            <div class="col-12">
+                                 <label id="fechaImg" class="text-muted"> <small>Hace 6 dias</small></label>
+                            </div>  
+                            <div class="col-12">
+                                 <label  id="palabrasClave" >
+                                    <span class="badge badge-pill border border-info px-2 py-1">Rio</span>
+                                    <span class="badge badge-pill border border-info px-2 py-1">Lago</span>
+                                    <span class="badge badge-pill border border-info px-2 py-1">Naturaleza</span>
+                                  </label>
+                            </div>
+                            <div class="col-12">
+                            <hr class="m-0 p-0">
+                                <!-- <textarea class="text-sans mt-1" id="historyImg" ></textarea> -->
+                                <textarea name="pueb" id="" cols="30" rows="10" class="form-control text-sans">Un león que vagaba por el bosque se clavó una espina en la pata, y al encontrar un pastor, le pidió que se la extrajera. 
 
-El pastor lo hizo, y el león, que estaba saciado porque acababa de devorar a otro pastor, siguió su camino sin hacerle daño. Algún tiempo después, el pastor fue condenado, a causa de una falsa acusación, a ser arrojado a los leones en el anfiteatro. Cuando las fieras estaban por devorarlo, una de ellas dijo:
-—Este es el hombre que me sacó la espina de la pata.
+    El pastor lo hizo, y el león, que estaba saciado porque acababa de devorar a otro pastor, siguió su camino sin hacerle daño. Algún tiempo después, el pastor fue condenado, a causa de una falsa acusación, a ser arrojado a los leones en el anfiteatro. Cuando las fieras estaban por devorarlo, una de ellas dijo:
+    —Este es el hombre que me sacó la espina de la pata.
 
-Al oír esto, los otros leones honorablemente se abstuvieron, y el que habló se comió él solo al Pastor.
-                            </textarea>
-                        </div>  
+    Al oír esto, los otros leones honorablemente se abstuvieron, y el que habló se comió él solo al Pastor.
+                                </textarea>
+                            </div>  
+                        </div>
                     </div>
                 </div>
+              </div>
+              <div class="modal-footer">
+                <button type="button" class="btn btn-danger" data-dismiss="modal">Cerrar</button>
+              </div>
             </div>
           </div>
-          <div class="modal-footer">
-            <button type="button" class="btn btn-danger" data-dismiss="modal">Cerrar</button>
-          </div>
         </div>
-      </div>
-    </div>
+    </form>
 @endsection
