c604eb981d13880d822e978ca2cd57fa4ceb0cfa
venixGit <69021103+venixGit@users.noreply.github.com>
2021-06-22 19:20:50 -0600

    feat: "ruta crearArticulos agregado"

--- routes/web.php
+++ routes/web.php
@@ -1,23 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ArticulosController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-// Route::view('/','index')->name('index');
-// Route::view('/galeria','galeria')->name('galeria');
-// Route::view('recuperacion','recuperacion')->name('recuperacion');
-
+// Route::view('/','home')->name('home');
 Route::view('/','home')->name('home')->middleware(['auth']);
 Route::get('/logout', '\App\Http\Controllers\Auth\LoginController@logout');
-// Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::post('/', [ArticulosController::class, 'store'])->name('crearArticulos');
+
+// Route::get('articulos', '\App\Http\Controllers\Auth\ArticulosController@crearArticulos');
+// Route::get('App/Http/Controllers',[ArticulosController::class, 'crearArticulos'])
+// ->name('crearArticulos');
 Auth::routes();
