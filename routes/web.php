6152b1ba76596a40e03b63bf1ac120dc1dcfe20b
venixGit <69021103+venixGit@users.noreply.github.com>
2021-06-24 18:29:32 -0600

    fix "correción de rutas"

--- routes/web.php
+++ routes/web.php
@@ -4,10 +4,16 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ArticulosController;
 
 // Route::view('/','home')->name('home');
-Route::view('/','home')->name('home')->middleware(['auth']);
+// Route::view('/','home')->name('home')->middleware(['auth']);
 Route::get('/logout', '\App\Http\Controllers\Auth\LoginController@logout');
-Route::post('/', [ArticulosController::class, 'store'])->name('crearArticulos');
 
+
+Route::get('/', [ArticulosController::class, 'mostrarArticulos'])->name('home')->middleware(['auth']);
+Route::post('/articulos', [ArticulosController::class, 'guardarArticulos'])->name('crearArticulos');
+// Route::get('/buscar', [ArticulosController::class, 'busquedas'])->name('buscarPalabra');
+
+
+// Route::get('/imagen/{$filename}', [ArticulosController::class, 'mostrarImg'])->name('mostrarImg');
 // Route::get('articulos', '\App\Http\Controllers\Auth\ArticulosController@crearArticulos');
 // Route::get('App/Http/Controllers',[ArticulosController::class, 'crearArticulos'])
 // ->name('crearArticulos');
